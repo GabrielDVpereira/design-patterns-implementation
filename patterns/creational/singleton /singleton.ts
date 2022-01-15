bd10d7b822db0a024602a6d6f0152600c6c07c15
gabrieldvpereira <gdavi.gd@gmail.com>
2022-01-15 18:04:29 -0300

    feat: add singleton

---
+++ patterns/creational/singleton /singleton.ts
@@ -0,0 +1,26 @@
+class Singleton {
+    private static instance: Singleton; 
+
+    private constructor() {} // prevents initialization with new operator outside of the class 
+
+    public static getInstance(){ 
+        if(!Singleton.instance){
+            Singleton.instance = new Singleton();
+        }
+
+        return Singleton.instance; // always sharing the same instance
+    }
+}
+
+
+function clientCode(){
+     const s1 = Singleton.getInstance(); 
+     const s2 = Singleton.getInstance(); 
+
+    if(s1 === s1){
+        console.log('singleton works!')
+        return; 
+    }
+
+    console.log('singleton does not work');
+}
