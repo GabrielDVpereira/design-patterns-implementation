ab6dbec14923f01ae40d5b3a756958f68de61932
gabrieldvpereira <gdavi.gd@gmail.com>
2022-01-18 22:08:44 -0300

    feat: add validator pattern

---
+++ patterns/structural/adapter/emailValidator.ts
@@ -0,0 +1,11 @@
+import { EmailValidator } from "./validator";
+
+export class EmailValidatorAdapter implements EmailValidator {
+    validate(email: string): boolean {
+        return !!String(email)
+                .toLowerCase()
+                .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                )?.length;
+    }
+}
---
+++ patterns/structural/adapter/init.ts
@@ -0,0 +1,38 @@
+import { EmailValidatorAdapter } from './emailValidator'; 
+import { EmailValidator } from './validator'
+
+type LoginCredentials = {
+    email: string, 
+    password: string, 
+}
+
+type SignUpCredentials = {
+    email: string, 
+    password: string, 
+    passwordConfirmation: string, 
+}
+
+class Application {
+    constructor(
+        private readonly emailValidator: EmailValidator
+    ){}
+
+   async login(credentials: LoginCredentials ): Promise<void> {
+        const isEmailValid = this.emailValidator.validate(credentials.email);
+        if(isEmailValid){
+            console.log('login')
+        } 
+   }
+
+   async signup(credentials: SignUpCredentials){
+        const isEmailValid = this.emailValidator.validate(credentials.email);
+        if(isEmailValid){
+            console.log('signup')
+        } 
+   }
+}
+
+const validator = new EmailValidatorAdapter(); 
+const app = new Application(validator); 
+
+// ... some auth logic stuff
