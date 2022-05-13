715ab172eb207af146296fef594cbee2047efd87
gabrieldvpereira <gdavi.gd@gmail.com>
2022-05-12 23:34:04 -0300

    chore: add comments to the factory method

--- patterns/creational/factory_method/init.ts
+++ patterns/creational/factory_method/init.ts
@@ -1,3 +1,17 @@
+/**
+ * DEFINITION
+ * Factory method let's you produce related classes that follow a common interface. 
+ * It's important to mantain the single reponsability principle. 
+ * Each class will be responsible for only one actor. Then we let the system decide which actor it is
+ */
+
+/**
+ *  IMPLEMENTATION
+ * 1. Create a common interface that all classes will implement. 
+ * 2. Create a 'factory', it's a class or a method that will return the interface. So the main app doesn't know about the concrete implementations
+ * 3. In the main app, call the factory asking for the type of class you need, then you can execute it's action
+ */
+
 import { INotification, INotificationData } from './Notification'; 
 import { AlertNotification } from './AlertNotification'; 
 import { ErrorNotification } from './ErrorNotification'; 
